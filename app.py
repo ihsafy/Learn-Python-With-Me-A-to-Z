c4c26317d0f1d2540964d9a2e1c05834d24ca822
IH Safy <101524119+ihsafy@users.noreply.github.com>
2025-01-18 19:27:10 +0600

    Update app.py
    This Is for the day 1.

--- app.py
+++ app.py
@@ -32,3 +32,20 @@ print(course.lstrip())
 
 # Removes only the trailing whitespace from the 'course' string
 print(course.rstrip())
+
+# Finds the first occurrence of the substring "Python" in the 'course' string and returns its index.
+# If "Python" is not found, it returns -1.
+print(course.find("Python"))
+
+# Replaces all occurrences of the substring "Python" with "Snake" in the 'course' string.
+# The modified string is returned, but the original 'course' string remains unchanged.
+print(course.replace("Python", "Snake"))
+
+# Checks if the substring "pro" exists in the 'course' string.
+# Returns True if found, otherwise returns False.
+print("pro" in course)
+
+# Checks if the substring "Safy" does NOT exist in the 'course' string.
+# Returns True if "Safy" is not found, otherwise returns False.
+print("Safy" not in course)
+
